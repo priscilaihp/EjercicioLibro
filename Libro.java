394c65ed6d5642c773ff7b2861237e61c6f25831
Priscila <witzilo.pihp@hotmail.es>
2017-08-24 10:46:03 -0500

    Título y Autor

---
+++ Libro.java
@@ -0,0 +1,11 @@
+public class Libro
+{
+    private String titulo;
+    private String autor;
+    
+    public Libro(String tituloLibro, String autorLibro)
+    {
+        titulo+=tituloLibro;
+        autor+=autorLibro;
+    }
+}
