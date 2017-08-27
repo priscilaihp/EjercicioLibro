7f10caa5211d86d04eef5aad96e40673a1739c78
Priscila <witzilo.pihp@hotmail.es>
2017-08-27 18:21:46 -0500

    Agregue dimeDetalles del Libro

--- Libro.java
+++ Libro.java
@@ -2,11 +2,13 @@ public class Libro
 {
     private String titulo;
     private String autor;
+    private int paginas;
     
-    public Libro(String tLibro, String aLibro)
+    public Libro(String tLibro, String aLibro, int pLibro)
     {
         titulo = tLibro;
         autor = aLibro;
+        paginas = pLibro;
     }
     
     String dimeAutor()
@@ -17,5 +19,24 @@ public class Libro
     String dimeTitulo()
     {
         return titulo;
+    }
+    
+    int dimePaginas()
+    {
+        return paginas;
+    }
+    
+    String dimeDetalles()
+    {
+        String datos = " ";
+        
+        datos += "Título:";
+        datos += titulo;
+        datos += ", Autor:";
+        datos += autor;
+        datos += ", Páginas:";
+        datos += paginas;
+        
+        return datos;
     }
 }
